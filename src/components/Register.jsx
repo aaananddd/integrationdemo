3c0ad7176ed68108eb78c5327d775cdba11186ea
ANAND <anandchandran3306@gmail.com>
2024-06-13 14:15:26 +0530

    changed some styles in register form.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -17,11 +17,11 @@ const RegisterForm = () => {
       className="w-[400px] mx-auto p-10 border border-gray-300 rounded-md bg-gray-100"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <h2 className="mb-20 font-bold text-3xl text-center">Register</h2>
+      <h2 className="mb-10 font-bold text-3xl text-center">Register</h2>
       <div className="mb-15">
         <label
           htmlFor="username"
-          className=" flex font-mono items-start block mb-5"
+          className=" flex font-mono items-start block mb-3"
         >
           Email:
         </label>
@@ -48,14 +48,14 @@ const RegisterForm = () => {
       <div className="mb-15">
         <label
           htmlFor="password"
-          className="flex items-start font-mono mt-5 block mb-5"
+          className="flex items-start font-mono mt-3 block mb-3"
         >
           Password:
         </label>
         <input
           type="password"
           id="password"
-          className={`input w-full p-3 border mb-5 border-gray-300 rounded-md box-border ${
+          className={`input w-full p-3 border mb-3 border-gray-300 rounded-md box-border ${
             errors.password && "input-error"
           }`}
           {...register("password", {
@@ -69,6 +69,33 @@ const RegisterForm = () => {
         {errors.password && (
           <span className="error-message text-red-500 text-xs">
             {errors.password.message}
+          </span>
+        )}
+      </div>
+      <div className="mb-15">
+        <label
+          htmlFor="confirmpassword"
+          className="flex items-start font-mono mt-3 block mb-3"
+        >
+          Confirm Password:
+        </label>
+        <input
+          type="password"
+          id="confirmpassword"
+          className={`input w-full p-3 border mb-5 border-gray-300 rounded-md box-border ${
+            errors.confirmpassword && "input-error"
+          }`}
+          {...register("confirmpassword", {
+            required: "Please re-enter your password",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters long",
+            },
+          })}
+        />
+        {errors.confirmpassword && (
+          <span className="error-message text-red-500 text-xs">
+            {errors.confirmpassword.message}
           </span>
         )}
       </div>
