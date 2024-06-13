11e31babccdb84061a1d738c1223e6266869614c
ANAND <anandchandran3306@gmail.com>
2024-06-13 15:53:17 +0530

    added register, login, dashboard  pages.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@ import React from "react";
 import Header from "./components/header";
 import "./App.css";
 import RegisterForm from "./components/Register";
+import LoginForm from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 const App = () => {
   return (
     <div className="navbar">
-      <Header />
-      <RegisterForm />
+      <Header/>
+      <div className="grid grid-cols-2 mt-12">
+        <div>
+        <RegisterForm />
+        </div>
+        <div>
+          <LoginForm />
+        </div>
+        <div>
+          <Dashboard />
+        </div>
+      </div>
+      
     </div>
   );
 };
---
+++ src/components/Login.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      className="w-[400px] mx-auto p-10 border border-gray-300 rounded-md bg-gray-100"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h2 className="mb-10 font-bold text-3xl text-center">Login</h2>
+      <div className="mb-15">
+        <label
+          htmlFor="username"
+          className=" flex font-mono items-start block mb-3"
+        >
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          className={`input w-full p-3 border border-gray-300 rounded-md box-border ${
+            errors.email && "input-error"
+          }`}
+          {...register("email", {
+            required: "Please enter your email",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+              message: "Please enter a valid email address",
+            },
+          })}
+        />
+        {errors.email && (
+          <span className="error-message text-red-500 text-xs">
+            {errors.email.message}
+          </span>
+        )}
+      </div>
+      <div className="mb-15">
+        <label
+          htmlFor="password"
+          className="flex items-start font-mono mt-3 block mb-3"
+        >
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          className={`input w-full p-3 border mb-3 border-gray-300 rounded-md box-border ${
+            errors.password && "input-error"
+          }`}
+          {...register("password", {
+            required: "Please enter your password",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters long",
+            },
+          })}
+        />
+        {errors.password && (
+          <span className="error-message text-red-500 text-xs">
+            {errors.password.message}
+          </span>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="submit-button w-full p-3 bg-blue-600 text-white rounded-md cursor-pointer text-lg"
+      >
+        Register
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
